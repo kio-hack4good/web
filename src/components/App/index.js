37353ea690ea627f075bc73b67d87217bc0ba4e9
Ferusel <wheatgrass.mc@gmail.com>
2023-02-11 05:24:31 +0800

    Fix more broken links

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,6 +5,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import { UserAuthContextProvider } from "../../context/UseAuthContext";
 import Intro from "../../pages/Intro";
 import Landing from "../../pages/Landing";
+import MapView from "../../pages/MapView";
 import RolePage from "../../pages/Role";
 import { lightTheme } from "../../themes/lightTheme";
 import Login from "../Auth/Login";
@@ -17,6 +18,14 @@ function App() {
       <CssBaseline />
       <UserAuthContextProvider>
         <Routes>
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <MapView />
+              </ProtectedRoute>
+            }
+          />
           <Route path={"/welcome"}>
             <Route index element={<Landing />} />
             <Route path={"login"} element={<Login />} />
@@ -24,14 +33,6 @@ function App() {
             <Route path={"role"} element={<RolePage />} />
             <Route path={"intro"} element={<Intro />} />
           </Route>
-          <Route
-            path="/"
-            element={
-              <ProtectedRoute>
-                <Landing />
-              </ProtectedRoute>
-            }
-          />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </UserAuthContextProvider>
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,13 +1,11 @@
 import { Grid } from "@mui/material";
 import Button from "@mui/material/Button";
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import logo from "../assets/logo.svg";
 
 export default function Landing() {
-  let navigate = useNavigate();
-
   return (
     <Grid
       container
