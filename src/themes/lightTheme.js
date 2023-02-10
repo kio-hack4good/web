a6db52c301e5dac99bad3cdb2f92f401708ce851
Ferusel <wheatgrass.mc@gmail.com>
2023-02-10 17:22:26 +0800

    Add light theme

---
+++ src/themes/lightTheme.js
@@ -0,0 +1,39 @@
+import { createTheme } from "@mui/material/styles";
+
+export const themeOptions = {
+  palette: {
+    type: "light",
+    primary: {
+      main: "#7454FB",
+    },
+    secondary: {
+      main: "#0A9AFF",
+    },
+    background: {
+      default: "#F2EEEE",
+    },
+  },
+  typography: {
+    fontFamily: "Lato",
+    fontSize: 16,
+    h1: {
+      fontSize: 32,
+    },
+    h2: {
+      fontSize: 26,
+    },
+    h3: {
+      fontSize: 22,
+    },
+    h4: {
+      fontSize: 20,
+    },
+    h5: {
+      fontSize: 13.3,
+    },
+    h6: {
+      fontSize: 10.7,
+    },
+  },
+};
+export const lightTheme = createTheme(themeOptions);
