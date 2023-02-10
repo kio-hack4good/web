d5bb8a7b9c45831bcdfbb8f026545fc485ee75a2
Ferusel <wheatgrass.mc@gmail.com>
2023-02-10 13:06:17 +0800

    Add skeleton for router

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,22 +1,14 @@
 import './index.css';
+import {Route, Routes} from "react-router-dom";
+import Home from "../../pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={ <Home/> } />
+        </Routes>
+      </div>
   );
 }
 
