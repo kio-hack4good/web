b1f2a9a6e056f0e0201b02711161e4afb35bd05e
Ferusel <wheatgrass.mc@gmail.com>
2023-02-10 18:22:17 +0800

    Add Register route

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,11 +4,16 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import HomePage from "../../pages/Home";
 import { lightTheme } from "../../themes/lightTheme";
+import Register from "../Auth/Register";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
   },
 ]);
 
