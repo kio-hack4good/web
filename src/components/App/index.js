44e0b28240e4606fb0e6f4191c09bf0d4f42a3e8
Ferusel <wheatgrass.mc@gmail.com>
2023-02-11 01:16:04 +0800

    Fix routes and protect authenticated routes

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,6 +1,6 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 import { UserAuthContextProvider } from "../../context/UseAuthContext";
 import HomePage from "../../pages/Home";
@@ -8,21 +8,6 @@ import { lightTheme } from "../../themes/lightTheme";
 import Login from "../Auth/Login";
 import Register from "../Auth/Register";
 import ProtectedRoute from "../ProtectedRoutes";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <HomePage />,
-  },
-  {
-    path: "/register",
-    element: <Register />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-]);
 
 function App() {
   return (
@@ -40,6 +25,7 @@ function App() {
           />
           <Route path={"/"} element={<Login />} />
           <Route path={"/register"} element={<Register />} />
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </UserAuthContextProvider>
     </ThemeProvider>
