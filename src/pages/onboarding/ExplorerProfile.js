67f259f9253b14113c09a7c79b9b634830ff1623
Ferusel <wheatgrass.mc@gmail.com>
2023-02-11 09:20:54 +0800

    Add explorer onboarding pages

--- src/pages/onboarding/ExplorerProfile.js
+++ src/pages/onboarding/ExplorerProfile.js
@@ -1,5 +1,118 @@
+import { Box, FormControlLabel, Radio, RadioGroup, Slider, Stack, Typography } from "@mui/material";
+import Button from "@mui/material/Button";
 import React from "react";
+import { Link } from "react-router-dom";
 
-const BefrienderProfile = () => {};
+const BefrienderProfile = () => {
+  return (
+    <Box
+      sx={{
+        backgroundColor: "background.default",
+        marginLeft: "5vw",
+        marginRight: "5vw",
+        marginBottom: "10vh",
+      }}>
+      <Typography
+        variant={"h1"}
+        sx={{ fontWeight: "600", textAlign: "center", marginTop: "10vh", marginBottom: "5vh" }}>
+        Let us get to know you!
+      </Typography>
+      <Stack spacing={3}>
+        <Box
+          sx={{
+            backgroundColor: "white",
+            width: "100%",
+            padding: "3em",
+            borderRadius: "15px",
+          }}>
+          <Typography
+            variant={"h2"}
+            sx={{
+              fontWeight: "600",
+              marginBottom: "0.5em",
+            }}>
+            How comfortable are you to be with befrienders at the moment?
+          </Typography>
+          <Typography
+            variant={"h5"}
+            sx={{
+              marginBottom: "1em",
+            }}>
+            Do not worry too much, we will allow you to change this later in-app!{" "}
+          </Typography>
+          <Slider defaultValue={3} marks={marks} step={null} valueLabelDisplay="auto" />
+        </Box>
+        <Box
+          sx={{
+            backgroundColor: "white",
+            width: "100%",
+            padding: "3em",
+            borderRadius: "15px",
+          }}>
+          <Typography
+            variant={"h2"}
+            sx={{
+              fontWeight: "600",
+              marginBottom: "0.5em",
+            }}>
+            What is your current interest level?
+          </Typography>
+          <Typography
+            variant={"h5"}
+            sx={{
+              marginBottom: "1em",
+            }}>
+            How interested are you in the activities the app is offering?
+          </Typography>
+          <Slider defaultValue={3} marks={marks} step={null} valueLabelDisplay="auto" />
+        </Box>
+        <Box
+          sx={{
+            backgroundColor: "white",
+            width: "100%",
+            padding: "3em",
+            borderRadius: "15px",
+          }}>
+          <Typography
+            variant={"h2"}
+            sx={{
+              fontWeight: "600",
+              marginBottom: "0.5em",
+            }}>
+            Are you willing to attend workshops?
+          </Typography>
+          <Typography
+            variant={"h5"}
+            sx={{
+              marginBottom: "1em",
+            }}>
+            Are you willing to attend workshops and activities? You will be notified of these
+            activities when they are available!
+          </Typography>
+          <RadioGroup>
+            <FormControlLabel value="yes" control={<Radio />} label={"Yes"} />
+            <FormControlLabel value="no" control={<Radio />} label={"No"} />
+          </RadioGroup>
+        </Box>
+
+        <Link to={"/"}>
+          <Button
+            variant={"outlined"}
+            sx={{
+              marginRight: "0",
+            }}>
+            <Typography
+              variant={"h3"}
+              sx={{
+                textTransform: "none",
+              }}>
+              Let&apos;s Go!
+            </Typography>
+          </Button>
+        </Link>
+      </Stack>
+    </Box>
+  );
+};
 
 export default BefrienderProfile;
--- src/pages/onboarding/ExplorerStatus.js
+++ src/pages/onboarding/ExplorerStatus.js
@@ -1,5 +1,143 @@
-import React from "react";
+import { AddAPhoto } from "@mui/icons-material";
+import { Box, IconButton, Input, Stack, Typography } from "@mui/material";
+import Button from "@mui/material/Button";
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-const BefrienderStatus = () => {};
+const BefrienderStatus = () => {
+  const [file, setFile] = useState(null);
+  const navigate = useNavigate();
+  const handleSubmit = (values) => {
+    console.log(values);
+    navigate("/onboarding/explorer-profile");
+  };
+  return (
+    <Box
+      sx={{
+        backgroundColor: "background.default",
+        marginLeft: "5vw",
+        marginRight: "5vw",
+      }}>
+      <Stack
+        spacing={2}
+        sx={{
+          marginTop: "10vh",
+          marginBottom: "10vh",
+        }}>
+        <div>
+          <Typography
+            variant={"h1"}
+            sx={{
+              fontWeight: "600",
+              textAlign: "center",
+            }}>
+            Verify your Identity
+          </Typography>
+          <Typography
+            variant={"h3"}
+            sx={{
+              textAlign: "center",
+            }}>
+            Take a picture of any of the following identifications:
+          </Typography>
+        </div>
+        <Stack
+          spacing={1}
+          sx={{
+            textAlign: "center",
+          }}>
+          <Typography variant={"h4"}>1. Disability Concession Card</Typography>
+          <Typography variant={"h4"}>
+            2. Developmental Disability Registry (DDR) Identity Card{" "}
+          </Typography>
+          <Typography variant={"h4"}>3. CaringSG Membership Card </Typography>
+        </Stack>
+        <Formik
+          initialValues={{
+            image: null,
+          }}
+          onSubmit={handleSubmit}>
+          <Form>
+            <Field name={"proof-of-identity"}>
+              {({ form, field }) => (
+                <Input
+                  id="fileInput"
+                  accept="image/png, image/jpeg"
+                  type="file"
+                  onChange={(e) => {
+                    setFile(e.target.files[0]);
+                    form.setFieldValue(field.name, e.currentTarget.files[0]);
+                  }}
+                  sx={{
+                    display: "none",
+                  }}
+                />
+              )}
+            </Field>
+            <ErrorMessage name="image">
+              {(msg) => (
+                <Typography
+                  variant={"h5"}
+                  sx={{
+                    color: "warning.main",
+                  }}>
+                  {msg}
+                </Typography>
+              )}
+            </ErrorMessage>
+            <Stack>
+              {file && (
+                <>
+                  <img
+                    style={{
+                      borderRadius: "10px",
+                      height: "30vh",
+                      width: "100%",
+                      objectFit: "cover",
+                    }}
+                    alt={"Proof of identity"}
+                    src={URL.createObjectURL(file)}
+                    onClick={() => document.getElementById("fileInput").click()}
+                  />
+                </>
+              )}
+              {!file && (
+                <Box
+                  onClick={() => document.getElementById("fileInput").click()}
+                  sx={{
+                    backgroundColor: "secondary.main",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyItems: "center",
+                    borderRadius: "10px",
+                    height: "30vh",
+                  }}>
+                  <IconButton
+                    variant={"outlined"}
+                    sx={{
+                      width: "100%",
+                    }}>
+                    <AddAPhoto />
+                    <Typography variant={"h5"}>Add a Profile Picture!</Typography>
+                  </IconButton>
+                </Box>
+              )}
+              <Button
+                variant="outlined"
+                type={"submit"}
+                sx={{
+                  marginTop: "5em",
+                  textTransform: "none",
+                }}>
+                Let&apos;s Continue!
+              </Button>
+            </Stack>
+          </Form>
+        </Formik>
+      </Stack>
+    </Box>
+  );
+};
 
 export default BefrienderStatus;
