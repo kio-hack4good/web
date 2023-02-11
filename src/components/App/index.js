112edbbd69f5add8595da92e62811760950d6e94
Ravern Koh <ravernkoh@gmail.com>
2023-02-11 10:04:13 +0800

    Fix breaking code

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -10,7 +10,7 @@ import MapViewPage from "../../pages/MapView";
 import BefrienderProfilePage from "../../pages/onboarding/BefrienderProfile";
 import BefrienderStatusPage from "../../pages/onboarding/BefrienderStatus";
 import ExplorerProfilePage from "../../pages/onboarding/ExplorerProfile";
-import ExplorerStatusPage from "../../pages/onboarding/ExplorerStatusPage";
+import ExplorerStatusPage from "../../pages/onboarding/ExplorerStatus";
 import RegisterPage from "../../pages/Register";
 import RolePage from "../../pages/Role";
 import { lightTheme } from "../../themes/lightTheme";
@@ -24,19 +24,19 @@ function App() {
         <Routes>
           <Route path="/" element={<ProtectedRoute />}>
             <Route index element={<MapViewPage />} />
-            <Route path={"/onboarding"}>
-              <Route path={"/befriender-status"} element={<BefrienderStatusPage />} />
-              <Route path={"/befriender-profile"} element={<BefrienderProfilePage />} />
-              <Route path={"/explorer-status"} element={<ExplorerStatusPage />} />
-              <Route path={"/explorer-profile"} element={<ExplorerProfilePage />} />
+            <Route path={"onboarding"}>
+              <Route path={"befriender-status"} element={<BefrienderStatusPage />} />
+              <Route path={"befriender-profile"} element={<BefrienderProfilePage />} />
+              <Route path={"explorer-status"} element={<ExplorerStatusPage />} />
+              <Route path={"explorer-profile"} element={<ExplorerProfilePage />} />
             </Route>
           </Route>
           <Route path={"/welcome"}>
             <Route index element={<LandingPage />} />
-            <Route path={"/login"} element={<LoginPage />} />
-            <Route path={"/register"} element={<RegisterPage />} />
-            <Route path={"/role"} element={<RolePage />} />
-            <Route path={"/intro"} element={<IntroPage />} />
+            <Route path={"login"} element={<LoginPage />} />
+            <Route path={"register"} element={<RegisterPage />} />
+            <Route path={"role"} element={<RolePage />} />
+            <Route path={"intro"} element={<IntroPage />} />
           </Route>
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
